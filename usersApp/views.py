12b33a83059204c6db7ad38616fafa33da6cca2a
hosea <hos97.ph@gmail.com>
2024-10-01 12:53:37 +0100

    01/10/2024

--- usersApp/views.py
+++ usersApp/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from .models import daily_task
 import datetime
 from datetime import datetime, timedelta
+import math
 # from django.utils import timezone
 from django.utils.timezone import make_aware, make_naive
 from django.db.models import Q
@@ -66,51 +67,61 @@ def trial(request):
  return render(request, "task.html", {'form':form})
 
 def userTask(request):
+ aware = make_aware(datetime.now())
+  # 14th september 2024 at 09:28AM
+ naive = make_naive(request.user.date_joined)
+ times=[naive,naive+timedelta(days=7)]
+ for time in times:
+  if time == times[0]:
+   starts = time.strftime("%d %B %Y at %I:%M%p")
+  else:
+   ends = time.strftime("%d %B %Y at %I:%M%p")
+  # greetings handler
+  time = datetime.now().hour
+  if time < 12:
+   greetings = "Morning"
+  elif time>12 and time<16:
+   greetings= "Afternoon"
+  else:
+   greetings= "Evening"
+
+  # retrieve request.user's essay records
+  records = daily_task.objects.filter(created_by=request.user).values('essay').count()
  if request.method == "POST":
   essay = testform(request.POST, request.FILES)
   if essay.is_valid():
-    aware = make_aware(datetime.now())
-    # 14th september 2024 at 09:28AM
-    starts =''
-    ends =''
-    naive = make_naive(request.user.date_joined)
-    times=[naive,naive+timedelta(days=7)]
-    for time in times:
-      if time == times[0]:
-       starts = time.strftime("%d %B %Y at %I:%M%p")
-      else:
-       ends = time.strftime("%d %B %Y at %I:%M%p")
-    # add item to database and test it
-    # greeting hander
-    time = datetime.now().hour
-    if time < 12:
-     greetings ='Morning'
-    elif time>12 and time<16:
-     greetings="Afternoon"
-    else:
-     greetings='Evening'
-    # record handler
-    record = daily_task.objects.filter(created_by=request.user).values('essay')
-    print(record)
+   record = daily_task.objects.filter(created_by=request.user).values('essay')
+   print(record)
 
-    cleaned_essay = essay.cleaned_data['essay']
-      # confirm if less than  days from the day of registration
-    if aware < request.user.date_joined + timedelta(days=15):
-     prev_time = make_aware(datetime.now()-timedelta(days=2))
-     essay_lessthan_24_hr_ago = daily_task.objects.filter(created_by=request.user).filter(Q(created_at__gt=prev_time) & Q(created_at__lte=aware))
-     if essay_lessthan_24_hr_ago:
-      essay_splitted = cleaned_essay.split()
-      sorted_essay = sorted(essay_splitted, key=len, reverse=True)
-      if len(sorted_essay[6]) >=4:
-       daily_task.objects.create(created_by=request.user,essay=cleaned_essay)
-       msg = 'Tests passed! essay saved to the database'
-      else:
-       msg = 'essay must contain atleast six-three letter words'
+   cleaned_essay = essay.cleaned_data['essay']
+    # confirm if less than days from the day of registration
+   aware = make_aware(datetime.now())
+   if aware < request.user.date_joined + timedelta(days=30):
+    prev_time = make_aware(datetime.now()-timedelta(minutes=2))
+    essay_lessthan_24_hr_ago = daily_task.objects.filter(created_by=request.user).filter(Q(created_at__gt=prev_time) & Q(created_at__lte=aware))
+    if essay_lessthan_24_hr_ago:
+     essay_splitted = cleaned_essay.split()
+     sorted_essay = sorted(essay_splitted, key=len, reverse=True)
+     if len(sorted_essay[6])>=4:
+      daily_task.objects.create(created_by=request.user,essay=cleaned_essay)
+      msg = 'Tests passed! essay saved to the database'
      else:
-      msg = 'Next essay must be 24hrs or more ago from the previous'
+      msg = 'essay must contain atleast six-three letter words'
     else:
+      # time restrain here!
+      dates_of_essay = daily_task.objects.order_by("-created_at").values('created_at')[0]['created_at']
+      time_remaining = datetime.now()-(make_naive(dates_of_essay)+timedelta(hours=3))
+      print(make_aware(time_remaining, utcoffset=True))
+      # make_aware(time_remaining).strftime
+      hours=math.floor(time_remaining.seconds/(3600))
+      minutes=time_remaining-hours*timedelta(seconds=3600)
+      print(time_remaining)
+      # sorted_dates_of_essay = sorted(dates_of_essay, reverse=True)
+      # print(sorted_dates_of_essay,'i am the date that as sorted')
+      msg = 'Next essay must be 2hrs or more from the previous essay'
+   else:
      msg = 'Your duration of tasks is exceeded'
   else:
    msg = 'Essay is invalid/empty!!'
   # dont forget to attach a message whenever the backend fails 
- return render(request, 'tasking.html', {'form':essay, 'msg':msg,'greet':greetings,'start':starts,'end':ends})       
+ return render(request, 'tasking.html', {'form':essay, 'msg':msg, 'starts':starts, 'ends':ends, 'greets':greetings, 'records':records})       
